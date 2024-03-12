f2366d045b61d4fb7573f5e3c255996fbc12236a
DaniDeDos <hasashih978@gmail.com>
2024-03-12 03:52:19 -0400

    add react-route-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import Nabvar from "./components/navbar.component";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Nabvar />
-    </>
+    <Routes>
+      <Route exact path="/" element={<Nabvar />}>
+        <Route exact path="/sigin" element={<h1>sig in</h1>} />
+        <Route exact path="/sigun" element={<h1>sig un</h1>} />
+      </Route>
+    </Routes>
   );
 };
 
